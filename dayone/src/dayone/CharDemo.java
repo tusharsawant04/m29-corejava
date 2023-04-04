50037516e0dd808a7077f3ef4621bc906060981e
tusharsawant04 <tsawant379@gmail.com>
2023-04-04 15:50:23 +0530

    second commit

---
+++ dayone/src/dayone/CharDemo.java
@@ -0,0 +1,17 @@
+package dayone;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='a';
+		System.out.println(ch);
+		//assigning number to character (A/c to ASCII table
+		char ch1=65;
+		System.out.println(ch1);
+		
+		char ch2='\u00A3';
+		System.out.println(ch2);
+	}
+
+}
--- dayone/src/dayone/FirstProgram.java
+++ dayone/src/dayone/FirstProgram.java
@@ -4,5 +4,6 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println();
 	}
 }
