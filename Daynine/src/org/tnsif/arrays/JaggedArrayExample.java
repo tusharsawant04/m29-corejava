127f2e7e3895cab43dc9cc00fdedc1cb3c53b245
tusharsawant04 <tsawant379@gmail.com>
2023-05-03 17:19:01 +0530

    commit

--- Daynine/src/org/tnsif/arrays/JaggedArrayExample.java
+++ Daynine/src/org/tnsif/arrays/JaggedArrayExample.java
@@ -1,22 +1,27 @@
 package org.tnsif.arrays;
-
+import java.util.Scanner;
 public class JaggedArrayExample {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		//in jagged array inside array size are different
-		int arr[][]=new int[2][];
-		
-		//for 1st block outside array,we are taking 3 elements
+		int arr[][] = new int[2][];
+		//for 1
 		arr[0]=new int[3];
 		//for 2nd block outside array, we are taking 2 elements
-		arr[1]=new int[2];
+		arr[1]= new int[2];
 		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0;i<arr.length;i++) {
-				
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j]=s.nextInt();
 			}
 		}
-		
+		for (int i = 0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 
 	}
 
